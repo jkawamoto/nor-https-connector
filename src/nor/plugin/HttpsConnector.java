13019d0ce54e4d3031ebd599ef7d9b80453043d0
jkawamoto <jkawamoto@9a145382-3267-40d2-a820-a830151a2666>
2010-06-21 09:16:46 +0000

    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/nor/HttpsConnectorPlugin/trunk@58 9a145382-3267-40d2-a820-a830151a2666

--- src/nor/plugin/HttpsConnector.java
+++ src/nor/plugin/HttpsConnector.java
@@ -1,3 +1,21 @@
+/**
+ *  Copyright (C) 2010 Junpei Kawamoto
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package nor.plugin;
 
 import java.util.regex.MatchResult;
@@ -35,7 +53,7 @@ public class HttpsConnector extends Plugin{
 		int i = 0;
 		for(; i != hosts.length-1; ++i){
 
-			regex.append(hosts[i].replace(".", "\\.").replace("*", ".*").replace("(", "(?:"));
+			regex.append(hosts[i].replace(".", "\\.").replace("*", "[^/]*").replace("(", "(?:"));
 			regex.append("|");
 
 		}
